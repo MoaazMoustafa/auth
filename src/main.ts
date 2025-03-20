fa5e34e34c4ee59721a922a1fffffbbeb8021cfa
Moaaz Moustafa <moaazmoustafa@gmail.com>
2025-03-20 23:01:44 +0200

    adding helmet and morgan logger

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
-  .setTitle('My API')
-  .setDescription('Automatically generated Swagger docs')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('My API')
+    .setDescription('Automatically generated Swagger docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api-docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  app.use(helmet());
   app.useLogger(new Logger());
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors();
-  const port = configService.get<number>('PORT') || 3000;
+  app.use(morgan('combined'));
+  const port = configService.get<number>('PORT') || 3000; 
   await app.listen(port);
 }
 bootstrap();
